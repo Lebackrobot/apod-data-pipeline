a236708c92868c9e2d206e4261c623484201f656
Lebackrobot <lebackman18@gmail.com>
2024-12-10 23:44:36 -0300

    chore: prod database connection

--- src/configs/db_connect.py
+++ src/configs/db_connect.py
@@ -3,12 +3,12 @@ import sqlalchemy
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(override=True)
 
 DATABASE_URL = getenv('DATABASE_URL')
 
 try:
-    db_engine = sqlalchemy.create_engine(DATABASE_URL)
+    db_engine = sqlalchemy.create_engine(DATABASE_URL, echo=True)
     db_connection = db_engine.connect()
 
 except Exception as error:
