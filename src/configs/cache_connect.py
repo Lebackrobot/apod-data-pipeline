adf07db3c3b94a0a6de83a7a90130d2ba45a33ea
Lebackrobot <lebackman18@gmail.com>
2024-12-16 09:41:58 -0300

    feat: redis integration

--- src/configs/cache_connect.py
+++ src/configs/cache_connect.py
@@ -1,9 +1,16 @@
 import redis
+from os import getenv
+
+CACHE_PASSWORD = getenv('CACHE_PASSWORD')
+CACHE_USERNAME = getenv('CACHE_USERNAME')
+CACHE_HOST = getenv('CACHE_HOST')
+CACHE_PORT = getenv('CACHE_PORT')
 
 cache_connection = redis.Redis(
-    host='localhost',
-    password='password',
-    port=6379, 
+    host=CACHE_HOST,
+    password=CACHE_PASSWORD,
+    username=CACHE_USERNAME,
+    port=CACHE_PORT,
     db=0, 
-    protocol=3
+    ssl = True 
 )
--- src/setup_cache_pipeline.py
+++ src/setup_cache_pipeline.py
@@ -1,10 +1,18 @@
 from src.apis.apod_api import ApodApi
 from src.configs.cache_connect import cache_connection
+from time import sleep
 
 class SetupCachePipeline:
     @staticmethod
     def handle_apod_api_stage():
-        return ApodApi.get()
+        try:
+            return ApodApi.get()
+    
+        except Exception as error:
+            print('APOD API endpoint error')
+            sleep(60)
+
+            SetupCachePipeline.handle_apod_api_stage()
         
 
     @staticmethod
@@ -21,8 +29,16 @@ class SetupCachePipeline:
         cache_connection.set('apod_url_image', apod_url_image)
         cache_connection.set('apod_title', apod_title)
 
+        print('SET apod_date')
+        print('SET apod_description')
+        print('SET apod_url_image')
+        print('SET apod_title')
+
+        print('SET values to redis (OK)')
+
 
     @staticmethod
     def run():
+        print('>>> run setup cache pipeline')
         response = SetupCachePipeline.handle_apod_api_stage()
         SetupCachePipeline.handle_cache_data_stage(response)
