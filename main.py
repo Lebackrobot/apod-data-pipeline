537594ae0d6a523d682cde67c79027e876e54f2a
Lebackrobot <lebackman18@gmail.com>
2024-12-16 09:42:13 -0300

    feat: schedule job

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import schedule
+import time
+
 from src.subscription_queuing_pipeline import SubscriptionQueuingPipeline
-from src.setup_cache_pipeline import SetupCachePipeline 
+from src.setup_cache_pipeline import SetupCachePipeline
+
 
 if __name__ == '__main__':
+    #schedule.every().day.at('08:00', 'America/Sao_Paulo').do(SetupCachePipeline.run)
+    #schedule.every().day.at('20:45', 'America/Sao_Paulo').do(SubscriptionQueuingPipeline.run)
+
+    print("Start pipeline service")
     #SubscriptionQueuingPipeline.run()
     SetupCachePipeline.run()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
--- src/subscription_queuing_pipeline.py
+++ src/subscription_queuing_pipeline.py
@@ -1,3 +1,5 @@
+import json
+
 from src.services.rabbit_service import RabbitService
 from src.services.subscriptionService import SubscriptionService
 
@@ -5,18 +7,19 @@ class SubscriptionQueuingPipeline:
     @staticmethod
     def handle_subscriptions_stage():
         return SubscriptionService.get_subscriptions()
-        
 
     @staticmethod
     def send_rabbit_message_stage(subscriptions):
         for subscription in subscriptions:
-            rabbitMessage = str(subscription)
+            rabbitMessage = json.dumps(subscription)
 
             print(f'>>> Send subscription email to rabbit: {subscription['to']}')
             RabbitService.send(rabbitMessage)
 
     @staticmethod
     def run():
+        print('>>> Run handle_subscriptions_stage')
         subscriptions = SubscriptionQueuingPipeline.handle_subscriptions_stage()
-        SubscriptionQueuingPipeline.send_rabbit_message_stage(subscriptions)
 
+        print('>>> Run send_rabbit_message_stage', subscriptions)
+        SubscriptionQueuingPipeline.send_rabbit_message_stage(subscriptions)
